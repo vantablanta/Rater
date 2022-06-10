fcbddc6d7fb35b6589e5905440e3b8673abcd023
vanta blanta <vantablanta@gmail.com>
2022-06-11 01:40:56 +0300

    api endpoints added on api homepage

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'Posts':'/post-list/',
+        'Profile': '/profile',
+        'Detail_view':'/post-detail/<str:pk>/',
+        'Create':'/post-create/',
+        'Update':'/post-update/<str:pk>/',
+        'Delete':'/post-delete/<str:pk>/',
+    }
+    return Response(api_urls)
